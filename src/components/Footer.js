f507b03bbf25b53df169fd53002c8543418df33e
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-10-30 07:48:14 -0600

    updated a few things flagged in netlify failed build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ const Footer = (props) => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="footer" style={{color: props.color}}>
+    <div className="footer" style={{ color: props.color }}>
       <div>
-      {props.flaticonLinks.map((link, index) => (
-        <a key={index} href={link.href} title={link.title} target='_blank'>
-          {link.text}{index < props.flaticonLinks.length - 1 ? '/' : ''}
-        </a> 
-      ))}
+        {props.flaticonLinks.map((link, index) => (
+          <a key={index} href={link.href} title={link.title} target='_blank' rel="noreferrer" >
+            {link.text}{index < props.flaticonLinks.length - 1 ? '/' : ''}
+          </a>
+        ))}
       </div>
-      <p style={{display: 'inline'}}>Icons created by {props.designers} - Flaticon</p>
-      <p>Copyright © {currentYear} Jeremy Krans. All Rights Reserved.</p>       
-      
+      <p style={{ display: 'inline' }}>Icons created by {props.designers} - Flaticon</p>
+      <p>Copyright © {currentYear} Jeremy Krans. All Rights Reserved.</p>
+
     </div>
   )
 }
