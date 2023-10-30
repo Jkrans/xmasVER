da0dfd2473c6fcfa0822f20857be1659cac3bcaf
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-10-30 08:00:34 -0600

    forgot to run build 2

--- src/components/ImageEquationRow.js
+++ src/components/ImageEquationRow.js
@@ -27,7 +27,7 @@ const ImageEquationRow = ({ onPass }) => {
         { img: mashed, value: getRandomNumber() },
       ]);
     }
-  }, []);
+  },);
 
   const handleInputChange = (index, event) => {
     const newInputValues = [...inputValues];
