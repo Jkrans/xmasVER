d8fe25c4eb9d5e5e34d3860366f6b29b767f8d56
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-11-13 08:03:07 -0700

    created background animation of icons 'floating' around for challenge four component

--- src/components/ChallengeFour.js
+++ src/components/ChallengeFour.js
@@ -1,28 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Story from './Story'
-import Headstone from './Headstone'
+import whisk from '../images/whisk.png'
+import knife from '../images/knife.png'
+import oven from '../images/oven.png'
 import TryAgain from './TryAgainMessage'
 
-const story = [
-    "After unlocking the cage in the basement, relief floods over you. Before you can turn to thank your skeletal companion, you find his cage eerily empty, with just a soft chuckle echoing in the background as a reminder of his presence. The dim light allows you to spot a set of worn-out cellar doors across the room. Hopeful, you make your way over and push them open. A gust of cool air greets you. Stepping out, you find yourself not in the safety of the outdoors as you hoped, but instead, in an eerie, moonlit graveyard. Fog blankets the ground, and twisted trees stretch their skeletal branches toward the sky. This is no ordinary graveyard, and as you will soon discover, its residents aren't exactly resting in peace.",
-    <br />,
-    <br />,
-    "Suddenly, from behind one of the tombstones, a figure emerges, draped in a bluish cloak that shimmers in the moonlight. She introduces herself as the \"Guardian of Lost Souls\", a spirit trapped in the graveyard for centuries. She explains, \"This graveyard is enchanted. The souls of those buried here remain restless, trapped in a limbo between realms. The only way to pacify them and ensure your safe passage is to solve their riddles.\" She continues, \"Solving these riddles won't just ensure your safe passage but will also bring some peace to these tormented souls.\"",
-    <br />,
-    <br />,
-    "The first tombstone starts to glow, indicating where you should begin. The Guardian of Lost Souls whispers, \"Remember, time is of the essence. With each riddle you solve, dawn approaches. You must solve them all before the first light touches the horizon, or you risk joining the souls here for eternity.\"",
-    <br />,
-    <br />,
-    "Your heart races as you take a deep breath. With determination, you step forward, ready to face the riddles of the graveyard and unlock the final path to freedom."
-]
+// const story = [
+//     "After unlocking the cage in the basement, relief floods over you. Before you can turn to thank your skeletal companion, you find his cage eerily empty, with just a soft chuckle echoing in the background as a reminder of his presence. The dim light allows you to spot a set of worn-out cellar doors across the room. Hopeful, you make your way over and push them open. A gust of cool air greets you. Stepping out, you find yourself not in the safety of the outdoors as you hoped, but instead, in an eerie, moonlit graveyard. Fog blankets the ground, and twisted trees stretch their skeletal branches toward the sky. This is no ordinary graveyard, and as you will soon discover, its residents aren't exactly resting in peace.",
+//     <br />,
+//     <br />,
+//     "Suddenly, from behind one of the tombstones, a figure emerges, draped in a bluish cloak that shimmers in the moonlight. She introduces herself as the \"Guardian of Lost Souls\", a spirit trapped in the graveyard for centuries. She explains, \"This graveyard is enchanted. The souls of those buried here remain restless, trapped in a limbo between realms. The only way to pacify them and ensure your safe passage is to solve their riddles.\" She continues, \"Solving these riddles won't just ensure your safe passage but will also bring some peace to these tormented souls.\"",
+//     <br />,
+//     <br />,
+//     "The first tombstone starts to glow, indicating where you should begin. The Guardian of Lost Souls whispers, \"Remember, time is of the essence. With each riddle you solve, dawn approaches. You must solve them all before the first light touches the horizon, or you risk joining the souls here for eternity.\"",
+//     <br />,
+//     <br />,
+//     "Your heart races as you take a deep breath. With determination, you step forward, ready to face the riddles of the graveyard and unlock the final path to freedom."
+// ]
 
 
 
 const ChallengeFour = ({ onPass }) => {
+    const [storyData, setStoryData] = useState(null);
     const [riddles, setRiddles] = useState([]);
     const [currentRiddleIndex, setCurrentRiddleIndex] = useState(0); // starts from the first riddle
     const [userInput, setUserInput] = useState('');
     const [tryAgainMessage, setTryAgainMessage] = useState(false);
+    const [scrollHeight, setScrollHeight] = useState(1801);
+
+    const whiskIconRef = useRef(null);
+    const knifeIconRef = useRef(null);
+
+    useEffect(() => {
+        async function fetchStory() {
+            try {
+                const response = await fetch("https://turkeyver-backend-production.up.railway.app/api/stories/3");
+                const data = await response.json();
+                setStoryData(data);
+            } catch (err) {
+                console.error("An error occurred while fetching story:", err);
+            }
+        }
+
+        fetchStory();
+    }, []);
 
     useEffect(() => {
         async function fetchRiddles() {
@@ -82,12 +103,12 @@ const ChallengeFour = ({ onPass }) => {
 
     useEffect(() => {
         // Set styles when the component mounts
-        document.body.style.background = 'linear-gradient(black, rgb(19, 63, 35))';
-        document.getElementsByClassName('header--h1')[0].style.color = 'rgb(19, 63, 35)';
+        document.body.style.background = 'linear-gradient(rgb(92, 132, 101)25%, rgb(50, 200, 125)';
+        document.getElementsByClassName('header--h1')[0].style.color = 'rgb(255, 255, 255, 0.8)';
 
         const footerLinks = document.querySelectorAll('.footer a, .footer p');
         footerLinks.forEach(link => {
-            link.style.color = "rgb(19, 63, 35)";
+            link.style.color = "rgb(50, 200, 125)";
         })
 
 
@@ -97,35 +118,78 @@ const ChallengeFour = ({ onPass }) => {
         };
     }, []);
 
+    const iconAnimate = (elementRef, duration, sh) => {
+        const element = elementRef.current;
+
+        const keyframes = [
+            { transform: `translateX(25vw) translateY(calc(${sh}px - 389px)) rotate(0deg)` },
+            { transform: 'translateX(-70vw) translateY(-30vh) rotate(180deg)' },
+            { transform: `translateX(-20vw) translateY(calc(${sh}px  - 389px)) rotate(90deg)` },
+            { transform: 'translateX(40vw) translateY(-30vh) rotate(270deg)' },
+            { transform: `translateX(25vw) translateY(calc(${sh}px  - 389px)) rotate(0deg)` }
+        ];
+
+        const timing = {
+            duration: duration, // Total animation duration in milliseconds
+            iterations: Infinity, // Repeat the animation forever
+            fill: 'forwards' // Ensure the animation stays at the last keyframe when finished
+        };
+
+        element.animate(keyframes, timing);
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const html = document.documentElement;
+            const height = Math.max(html.clientHeight, html.scrollHeight, html.offsetHeight);
+            setScrollHeight(height);
+            console.log("height: ", scrollHeight);
+            if (knifeIconRef) {
+                iconAnimate(knifeIconRef, 43000, scrollHeight)
+            }
+        }, 1000)
+        return () => clearTimeout(timer);
+    }, [scrollHeight])
+
     return (
         <div className="main--witch">
-            <Story title="The Graveyard" story={story} width="78%" color="rgb(255, 100, 25)" />
+            {storyData && <Story title={storyData.title} story={storyData.story} color="white" width="78%" />}
             {riddles.length > 0 && (
                 <>
-                    <div className="headstones">
+                    {/* <div className="headstones">
                         <Headstone isGlowing={true} />
                         <Headstone isGlowing={riddles[0].isSolved ? true : false} />
                         <Headstone isGlowing={riddles[1].isSolved ? true : false} />
                         <Headstone isGlowing={riddles[2].isSolved ? true : false} />
+                    </div> */}
+                    <div ref={whiskIconRef} class="floating-icon">
+                        <img src={whisk} alt="Floating Icon" />
                     </div>
-                    <div className="centerItems graveyard-riddle">
-                        <p>{riddles[currentRiddleIndex].question}</p>
-                        <form onSubmit={handleSubmit}>
-                            <input type='text' value={userInput} onChange={handleInputChange} maxLength={35} />
-                        </form>
-                        <TryAgain
-                            message='Please try again. Remember to check your spelling.'
-                            isDisplayed={tryAgainMessage}
-                            marginTop='1rem'
-                            color='black'
-                        />
+                    <div ref={knifeIconRef} class="floating-icon-2">
+                        <img src={knife} alt="Floating Icon" />
                     </div>
-                    <div className="headstones">
+                    <div className='ch4-riddles-container'>
+                        <img src={oven} alt="oven Icon" style={{ width: '45%' }} />
+                        <div className="ch4-riddle">
+                            <p>{riddles[currentRiddleIndex].question}</p>
+                            <form onSubmit={handleSubmit}>
+                                <input type='text' value={userInput} onChange={handleInputChange} maxLength={35} />
+                            </form>
+                            <TryAgain
+                                message='Please try again. Remember to check your spelling.'
+                                isDisplayed={tryAgainMessage}
+                                marginTop='1rem'
+                                color='black'
+                            />
+                        </div>
+                    </div>
+
+                    {/* <div className="headstones">
                         <Headstone isGlowing={riddles[3].isSolved ? true : false} />
                         <Headstone isGlowing={riddles[4].isSolved ? true : false} />
                         <Headstone isGlowing={riddles[5].isSolved ? true : false} />
                         <Headstone isGlowing={riddles[6].isSolved ? true : false} />
-                    </div>
+                    </div> */}
                 </>
             )}
         </div>
