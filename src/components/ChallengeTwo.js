002b128938c75683fb9ec4e75befe7937f4e4731
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-12-05 16:06:35 -0700

    forgot build for the 100th time

--- src/components/ChallengeTwo.js
+++ src/components/ChallengeTwo.js
@@ -140,6 +140,7 @@ const ChallengeTwo = ({ onPass }) => {
                         userInput={userInput}
                         handleInputChange={handleInputChange}
                         tryAgainMessage={tryAgainMessage}
+
                     />
 
                 </>
